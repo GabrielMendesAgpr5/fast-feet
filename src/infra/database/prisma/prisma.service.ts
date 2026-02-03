3998bd2362a67fd12d3bbc465e661d297455b944
gabriel.mendes <gabriel.mendes@agpr5.com>
2026-02-03 09:15:04 -0300

    :sparkles: User method working with debug mode

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 @Injectable()
 export class PrismaService
@@ -7,10 +8,16 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    })
+
     super({
+      adapter,
       log: ['warn', 'error'],
     })
   }
+
   async onModuleInit() {
     await this.$connect()
   }
